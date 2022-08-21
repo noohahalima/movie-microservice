9211dd08a3daefd11f6e459d98402a4cb1136b80
nuha halima <noohahalima@gmail.com>
2022-08-21 10:38:51 +0530

    adding all files

---
+++ ratings-data-service/ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,32 @@
+package io.javabrains.ratingsdataservice.resources;
+
+import io.javabrains.ratingsdataservice.model.Rating;
+import io.javabrains.ratingsdataservice.model.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId")String userId)
+    {
+        List<Rating> ratings= Arrays.asList(
+                new Rating("100",4),
+                new Rating("200",3)
+        );
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+    }
+}
